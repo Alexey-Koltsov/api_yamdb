5554bab1f762841b6a4f0c556be3da6cc45256a0
Pavel Okhrim <133741770+d1g-1t@users.noreply.github.com>
2023-10-28 20:09:40 +0300

    Permissions.py updated.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,12 +1,14 @@
 from rest_framework import permissions
 
+from reviews.models import User
+
 
 class IsAdmin(permissions.BasePermission):
     """Проверка: запрос произведен админом или суперпользователем."""
 
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
-            request.user.is_admin or request.user.is_superuser)
+            request.user.role == User.ADMIN or request.user.is_superuser)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
@@ -16,9 +18,11 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or (request.user.is_authenticated and (
-                    request.user.is_admin or request.user.is_superuser)))
+        return (
+            request.method in permissions.SAFE_METHODS
+            or (request.user.is_authenticated and (
+                request.user.role == User.ADMIN or request.user.is_superuser))
+        )
 
 
 class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
@@ -28,11 +32,15 @@ class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_admin
-                or request.user.is_moderator
-                or obj.author == request.user)
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.role == User.ADMIN
+            or request.user.role == User.MODERATOR
+            or obj.author == request.user
+        )
 
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
