445af3a05979927b898d99911ada0e3fb1b36f3d
Pavel Okhrim <133741770+d1g-1t@users.noreply.github.com>
2023-11-02 13:24:29 +0300

    Permissions.py updated.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -9,8 +9,7 @@ class IsAdmin(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and (
-            request.user.is_admin or request.user.is_superuser)
+        return request.user.is_authenticated and request.user.is_admin
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
@@ -22,8 +21,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated and (
-                request.user.is_admin or request.user.is_superuser))
+            or (request.user.is_authenticated and request.user.is_admin)
         )
 
 
@@ -36,7 +34,7 @@ class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user.is_admin   
+            or request.user.is_admin
             or request.user.is_moderator
             or obj.author == request.user
         )
